dbabe145b0f328745af52873b0d786b8a1860a0a
cauezinatto <95114605+cauezinatto@users.noreply.github.com>
2024-04-29 16:51:24 -0300

    Delete zip (1) directory

--- zip (1)/src/main/java/testeItau/repository/AutomovelRepository.java
+++
@@ -1,8 +0,0 @@
-package testeItau.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import testeItau.entity.Automovel;
-
-public interface AutomovelRepository extends JpaRepository<Automovel, Long> {
-
-}
